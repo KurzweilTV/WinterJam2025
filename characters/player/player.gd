7aaac2d5bdd2a709bde321581b4927bbd9acff95
Mike Barr <m.barr@mac.com>
2025-12-28 08:57:02 +0100

    adds player scene

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
 @export var speed = 50
+var last_direction: Vector2 = Vector2.DOWN # Track last direction for idle facing
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
-# Track last direction for idle facing
-var last_direction: Vector2 = Vector2.DOWN
+@onready var candle_light: PointLight2D = $CandleLight
 
 func get_input():
 	var input_direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
 	set_animation(input_direction)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_candle"):
+		toggle_candle()
 
 func _physics_process(_delta):
 	get_input()
@@ -32,3 +35,7 @@ func set_animation(direction: Vector2) -> void:
 			animated_sprite_2d.play("walk_down")
 		else:
 			animated_sprite_2d.play("walk_up")
+
+func toggle_candle() -> void:
+	candle_light.visible = !candle_light.visible
+	#TODO Candle Sounds
